196951cac5d0c01058c8a50723d65f978f6a9a4f
Ricardo Aguilar <reacxd579@gmail.com>
2023-10-28 10:05:30 -0600

    coomit

---
+++ src/app/Components/tareas/tareas.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Tarea } from 'src/app/Models/Tarea';
+
+@Component({
+  selector: 'app-tareas',
+  templateUrl: './tareas.component.html',
+  styleUrls: ['./tareas.component.css']
+})
+export class TareasComponent {
+ nombreTarea = "";
+ listaTareas: Tarea[] = [];
+
+
+ nuevaTarea(){
+console.log(this.nombreTarea)
+
+const tarea: Tarea ={
+  nombre: this.nombreTarea,
+  estado: false
+}
+
+
+this.listaTareas.push(tarea);
+this.nombreTarea = "";
+console.log(this.listaTareas)
+
+ }
+
+ actualizarTarea(tarea: Tarea, index: number){
+this.listaTareas[index].estado = !tarea.estado;
+ }
+
+
+ eliminarTarea(index: number){
+this.listaTareas.splice(index,1);
+ }
+
+}
